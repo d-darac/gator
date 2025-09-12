720748b1f6bb662fc79446a78f4ec30a930e5889
d-darac <dragicevic.dario@gmail.com>
2025-09-12 13:16:42 +0100

    modify scrapeFeeds to save fetched feed items as posts in db

--- handler_aggregate.go
+++ handler_aggregate.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 	"time"
+
+	"github.com/d-darac/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAggregate(s *state, cmd command) error {
@@ -25,28 +30,52 @@ func handlerAggregate(s *state, cmd command) error {
 	}
 }
 
-func scrapeFeeds(s *state) error {
+func scrapeFeeds(s *state) {
 	nextToFetch, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't get feed: %w", err)
+		log.Printf("couldn't get feed: %v", err)
+		return
 	}
 
 	log.Println("found a feed to fetch")
 
 	err = s.db.MarkFeedFetched(context.Background(), nextToFetch.ID)
 	if err != nil {
-		return fmt.Errorf("couldn't update feed: %w", err)
+		log.Printf("couldn't update feed: %v", err)
+		return
 	}
 
 	rssFeed, err := fetchFeed(context.Background(), nextToFetch.Url)
 	if err != nil {
-		return err
+		log.Print(err)
+		return
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf("      Found post: %s\n", item.Title)
+		publishedAt := sql.NullTime{}
+		if t, err := time.Parse(time.RFC1123Z, item.PubDate); err == nil {
+			publishedAt = sql.NullTime{
+				Time:  t,
+				Valid: true,
+			}
+		}
+
+		_, err := s.db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: item.Description,
+			Url:         item.Title,
+			PublishedAt: publishedAt,
+			FeedID:      nextToFetch.ID,
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				continue
+			}
+			log.Printf("couldn't create post: %v", err)
+		}
 	}
 	log.Printf("feed %s collected, %v posts found", nextToFetch.Name, len(rssFeed.Channel.Item))
-
-	return nil
 }
