c77fa39c6d22ea236c4ec0fdf38cef02efd6d3b1
d-darac <dragicevic.dario@gmail.com>
2025-09-12 11:52:25 +0100

    complete feature - agg command
    Squashed commit of the following:
    
    commit ebb629dbd2825d9ed34e4f8a5077450853ed2361
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Fri Sep 12 11:38:50 2025 +0100
    
        refactor MarkFeedFetched function call, add/update console log calls
    
    commit 921b84fcfee7d21b973bcbb2d5404694ed6c0663
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Fri Sep 12 11:36:49 2025 +0100
    
        refactor MarkFeedFetched function
    
    commit 89abbc5a7ae377a98e7687dac85976ab787c2ac9
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Fri Sep 12 11:36:22 2025 +0100
    
        update MarkFeedFetched to use builtin postgres NOW() function to set timestamp
    
    commit 5974c766509f2f1a9ed2d9e934c681890b73d707
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Fri Sep 12 11:12:52 2025 +0100
    
        update handlerAggregate function
    
    commit 9e6191886202ae4ca64f02a5e92e81eca0394de7
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Fri Sep 12 11:11:00 2025 +0100
    
        modify MarkFeedFetched to also update updated_at field
    
    commit 9a547ee796b7b450feec88f6497127e215c65741
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Thu Sep 11 15:27:12 2025 +0100
    
        add MarkFeedFetched. GetNextFeedToFetch functions;
        update CreateFeed, GetFeedByUrl, GetFeeds functions
    
    commit 5f88ff9a242ac95554e9aac9b4f60c603de4f738
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Thu Sep 11 15:25:42 2025 +0100
    
        add MarkFeedFetched and GetNextFeedToFetch queries
    
    commit 7084d7db8b914b32772a259b724eac45dec5a6cd
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Thu Sep 11 15:25:28 2025 +0100
    
        update Feed model
    
    commit ced04215bd75c03ddc105a602e93bfc11db8ba4e
    Author: d-darac <dragicevic.dario@gmail.com>
    Date:   Thu Sep 11 15:06:54 2025 +0100
    
        add migration

--- handler_aggregate.go
+++ handler_aggregate.go
@@ -3,13 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 )
 
 func handlerAggregate(s *state, cmd command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("usage: %v <time_between_reqs>", cmd.Name)
+	}
+
+	timeBetweenReqs, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't parse duration: %w", err)
+	}
+
+	log.Printf("collecting feeds every %v", timeBetweenReqs.String())
+
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	nextToFetch, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feed: %w", err)
+	}
+
+	log.Println("found a feed to fetch")
+
+	err = s.db.MarkFeedFetched(context.Background(), nextToFetch.ID)
+	if err != nil {
+		return fmt.Errorf("couldn't update feed: %w", err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), nextToFetch.Url)
 	if err != nil {
 		return err
 	}
-	fmt.Println(*feed)
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("      Found post: %s\n", item.Title)
+	}
+	log.Printf("feed %s collected, %v posts found", nextToFetch.Name, len(rssFeed.Channel.Item))
+
 	return nil
 }
